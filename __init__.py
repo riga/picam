d1beca77c775e68636aafb1f0374626e3310532d
Marcel Rieger <marcelrieger@me.com>
2013-07-15 21:46:24 +0200

    Add convenience.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os, random
 
 __all__ = ["PiCam"]
 
@@ -8,11 +8,12 @@ class PiCam(object):
     PHOTOCMD = "raspistill"
     VIDEOCMD = "raspivid"
 
-    def __init(self, path=None):
+    def __init__(self, path=None, shortArgs=False):
         if path:
             path = os.expandvars(os.expanduser(path))
             os.chdir(path)
         self.path = path
+        self.shortArgs = shortArgs
 
     def help(self, mode):
         if mode.lower() == "photo":
@@ -26,19 +27,22 @@ class PiCam(object):
     def __record(self, mode, name=None, path=None, timeout=0, **kwargs):
         if mode not in [PiCam.PHOTOCMD, PiCam.VIDEOCMD]:
             raise Exception("unknown mode: '%s'" % mode)
-         
+        
+        if not name:
+            name = "img_%s.jpg" % random.randint(1, 1000)
         target = name
         if path:
             path = os.expandvars(os.expanduser(path))
             target = os.path.join(path, name)
         
         parts = [mode, "-o %s -t %s" % (target, timeout)]
+        indicator = "-" if self.shortArgs else "--"
         for key, value in kwargs.items():
             if isinstance(value, bool):
             	if value:
-                    parts.append("--%s" % key)
+                    parts.append("%s%s" % (indicator, key))
             else:
-                parts.append("--%s %s" % (key, value))
+                parts.append("%s%s %s" % (indicator, key, value))
         
         cmd = " ".join(parts)
         os.system(cmd)
