75c86401cc27e022e4576c6b16bcde2e5a2b558f
Marcel R. <marcelrieger@me.com>
2013-07-15 18:22:15 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,3 +5,9 @@ __all__ = []
 class PiCam(object):
     def __init(self):
         pass
+
+    def photo(self, name=None, path=None, **kwargs):
+        pass
+    
+    def video(self, name=None, path=None, **kwargs):
+        pass
